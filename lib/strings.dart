aef16b7f5c74be9b68367397480b9af9ccef210c
ZeeshanWazirKhan <zeerockyf5@gmail.com>
2022-11-06 14:01:36 -0800

    My First Commit

---
+++ lib/strings.dart
@@ -0,0 +1,33 @@
+List<String> imgUrl = [
+  '1.jpg',
+  '2.jpg',
+  '3.jpg',
+  '4.jpg',
+  '5.jpeg',
+  '6.jpeg',
+  '7.jpeg',
+  '8.jpg',
+  '9.jpeg',
+];
+List<String> imgNames = [
+  'john_c',
+  'brock_l',
+  'shawn_m',
+  'e_Musk',
+  'john_c',
+  'brock_l',
+  'shawn_m',
+  'e_musk',
+  'e_musk'
+];
+List<String> captions = [
+  'Time to do something fun.....',
+  'Let\'s do it.....',
+  'Just have faith.....',
+  'Time to do something fun.....',
+  'Let\'s do it.....',
+  'Just have faith.....',
+  'Time to do something fun.....',
+  'Let\'s do it.....',
+  'Just have faith.....',
+];
